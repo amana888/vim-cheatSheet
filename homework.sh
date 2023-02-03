06048e8b739eaa9d367b5a205314044ac6ce2238
seytechdevops <124209666+seytechdevops@users.noreply.github.com>
2023-02-03 11:03:55 -0600

    Create homework.sh

---
+++ homework.sh
@@ -0,0 +1,9 @@
+
+#Hey guys, 
+#You will need to solve a small issue while trying to run the current script in your terminal
+#Attention: you will need to compare current file with the rest of older file in this vim-cheatsheet repository
+#and include missing part to be able to run it as a script
+
+#Creates a new variable with a value of "Hello World"
+learningbash="Hello World"
+echo $learningbash
